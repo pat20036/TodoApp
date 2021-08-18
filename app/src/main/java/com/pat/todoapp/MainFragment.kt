2d9246eb68e7907d0de44f3c8e6dbcdb9f4cf1ce
Patryk <pat20036@o2.pl>
2021-08-18 20:44:57 +0200

    Added base view/setting

---
+++ app/src/main/java/com/pat/todoapp/MainFragment.kt
@@ -0,0 +1,24 @@
+package com.pat.todoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pat.todoapp.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+}
